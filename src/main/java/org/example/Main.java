1a1fae6d024f1ed1c52b124c81d869c92e7eb232
Alan Roi <alanroira@gmail.com>
2022-07-12 11:54:39 -0500

    sub method returns BigInteger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,8 +15,8 @@ public class Main {
         return a*b;
     }
 
-    public static int sub(int a,int b){
-        return a-b;
+    public static BigInteger sub(int a,int b){
+        return BigInteger.valueOf(a).subtract(BigInteger.valueOf(b));
     }
     public static int div(int a,int b){
         return a/b;
