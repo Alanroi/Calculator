41c33e88890a245af5b01d18cb0681a11946fc2e
Alan Roi <alanroira@gmail.com>
2022-07-12 11:03:52 -0500

    Adding Add method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("the calculator is ready");
+        System.out.println(Add(1,6));
+    }
+
+    public static int Add(int a,int b){
+        return a+b;
     }
 }
